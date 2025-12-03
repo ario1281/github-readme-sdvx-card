2a10ef51e9dd313b7aa58beef55e0c810b012d51
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-12-03 10:05:50 +0900

    Update api_handler.js

--- ctrls/api_handler.js
+++ ctrls/api_handler.js
@@ -1,6 +1,5 @@
-import fetch from "node-fetch";
 import { Svg } from "../helpers/svg_helper.js";
-import { PlayerInfoToJson } from "../helpers/html_json_helper.js";
+import { VaddictInfo } from "../helpers/convert_html_helper.js";
 
 // cache two hours
 const CACHE_MAX_AGE = 7200;
@@ -13,22 +12,22 @@ export const apiHeaders = new Headers({
 export async function ApiHandler(req) {
   // Parse query parameters
   const { id } = req || {};
-  const apiUrl = `https://vaddict.b35.jp/user.php?player_id=${id}`;
+  const vaddict = new VaddictInfo();
 
   try {
-    const res = await fetch(apiUrl);
-    const player = PlayerInfoToJson(await res.text());
+    await vaddict.init(`https://vaddict.b35.jp/user.php?player_id=${id}`);
+    const player = vaddict.getPlayerInfo();
 
     // Parse parameters
     const param = {
       "id":   id,
-      "name": player[0],
+      "name": player["name"],
       "vf":   20.000,
-      "skill": player[3],
-      "played": player[4],
+      "skill": player["skill"],
+      "played": player["played"],
     };
 
-    const content = Svg.render({}); // render image for svg
+    const content = Svg.render(param); // render image for svg
 
     // sucsess
     return new Response(content, {
