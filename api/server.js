49859154e3f20606642f4d16f3a44a3acd70b77e
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-12-23 14:59:27 +0900

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,8 @@
+import fastify from "fastify";
+import serverless from "serverless-http";
 import { ApiHandler } from "../ctrls/api_handler.js";
+
+const app = fastify();
 
 async function SendFastifyResponse(reply, res) {
     // get text
@@ -10,17 +14,25 @@ async function SendFastifyResponse(reply, res) {
     reply.code(res.status).send(text);
 }
 
-
+app.get("/api", async (req, reply) => {
+    // return Response
+    const res = await ApiHandler(req.query);
+    await SendFastifyResponse(reply, res);
+});
 
 // ====== Local debug ======
+if (!process.env.VERCEL) {
+    app.listen({ port: 3000 }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log("Local Fastify server running at:", address);
+    });
+}
 
-
-//
-export default {
-  fetch(req) {
-    return new Response('Hello from Vercel!');
-  },
-};
+// ====== Serverless ======
+export default serverless(app);
 
 // end of api/server.js
 
